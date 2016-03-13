74d4bf4abbc93282a54558a95da101edea5713a3
licheebrick <licheebrick@163.com>
2016-03-13 23:30:03 +0800

    add apply function of BDD

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ class BDD(object):
     """
 
     def __init__(self):
-        self.s = ''
         self.nodelist = dict()
         self.nextnode = 0
         #self.ordering = dict()
@@ -37,7 +36,6 @@ class BDD(object):
         :return:
         """
         print 'Constructing, the string is %s'%s
-        self.s = s
         self.varnum = s.__len__()
         first = -1
         for bit in s:
@@ -55,6 +53,7 @@ class BDD(object):
             self.nodelist[0] = (first, 2, 1)
             self.nodelist[1] = (self.varnum, True)
             self.true = 1
+        self.nodenum = 2
 
         #print self.nodelist
         #print '\n'
@@ -69,11 +68,13 @@ class BDD(object):
             elif bit == '1':
                 #print '%d th level is %s'%(self.level, '1')
                 self.nextnode += 1
+                self.nodenum += 1
                 self.nodelist[self.nextnode - 1] = (self.level, self.false, self.nextnode)
 
             elif bit == '0':
                 #print '%d th level is %s'%(self.level, '0')
                 self.nextnode += 1
+                self.nodenum += 1
                 self.nodelist[self.nextnode - 1] = (self.level, self.nextnode, self.false)
 
             else:
@@ -88,7 +89,25 @@ class BDD(object):
         else:
             self.nodelist[self.nextnode] = (self.level, True)
             self.true = self.nextnode
+        self.nodenum += 1
         print self.nodelist
+
+    def __op__(self, op, oper1, oper2):
+        """
+
+        :param op:
+        :param oper1:
+        :param oper2:
+        :return:
+        """
+        if op in ('|', 'or'):
+            return oper1 | oper2
+        elif op in ('&', 'and'):
+            return oper1 & oper2
+        elif op in ('^', 'xor'):
+            return oper1 ^ oper2
+        else:
+            raise Exception('unknown operator "{op}"'.format(op = op))
 
     def apply(self, op, bdd1, bdd2):
         """
@@ -99,10 +118,9 @@ class BDD(object):
         :return:
         """
         self.varnum = bdd1.varnum
-        value = self.__apply__(op, bdd1, bdd2, 0, 0)
-        self.nodelist[0] = value
+        self.__apply__(op, bdd1, bdd2, 0, 0)
 
-    def __apply__(self, op, bdd1, bbd2, n1, n2):
+    def __apply__(self, op, bdd1, bdd2, n1, n2):
         """
 
         :param op:
@@ -110,22 +128,79 @@ class BDD(object):
         :param bbd2:
         :return: truple
         """
-        stack = list()
-        flag = list()
-        stack.append((0, 0, 0))  #(nodenum, bdd1where, bdd2where)
-        flag[0] = 0
-        node_bdd1 = bdd1.nodelist[0]
-        node_bdd2 = bdd2.nodelist[0]
-        if node_bdd1[0] == node_bdd2[0]:
-            stack.append((1, node_bdd1[2], node_bdd2[2]))
-            stack.append((2, node_bdd1[1], node_bdd2[1]))
+        thisnode = self.nextnode
+        print 'Generate %d node, with %d and %d th node of two bdd'%(thisnode, n1, n2)
+        self.nextnode += 1
+        bdd1node = bdd1.nodelist[n1]
+        bdd2node = bdd2.nodelist[n2]
+        print 'bdd1node:'
+        print bdd1node
+        print 'bdd2node:'
+        print bdd2node
+        if bdd1node[0] == bdd2node[0]:
+            print 'Same level;'
+            if len(bdd1node) == 2:
+                self.nodelist[thisnode] = (self.varnum,
+                                           self.__op__(op, bdd1node[1], bdd1node[1]))
+            else:
+                self.nodelist[thisnode] = (bdd1node[0],
+                                           self.__apply__(op, bdd1, bdd2, bdd1node[1], bdd2node[1]),
+                                           self.__apply__(op, bdd1, bdd2, bdd1node[2], bdd2node[2]))
+        elif bdd1node[0] > bdd2node[0]:
+            print 'Different level, 2 higher than 1;'
+            if not bdd1node[2]:
+                if not bdd1node[1]:
+                    if op in ('&', 'and'):
+                        print 'No need to cal deeper;'
+                        self.nodelist[thisnode] = (self.varnum, False)
+                    else:  #suppose we never use xor
+                        print 'copy the rest of bdd2;'
+                        self.nodelist[thisnode] = (bdd2node[0],
+                                           self.__apply__(op, bdd1, bdd2, n1, bdd2node[1]),
+                                           self.__apply__(op, bdd1, bdd2, n1, bdd2node[2]))
+                    #TODO:still need to implement xor
 
-        while stack:
+                else:
+                    if op in ('|', 'or'):
+                        print 'No need to cal deeper;'
+                        self.nodelist[thisnode] = (self.varnum, True)
+                    else: #suppose we never use xor
+                        #print 'copy the rest of bdd2'
+                        #TODO: FIND A SMARTER WAY TO DO THE COPY
+                        self.nodelist[thisnode] = (bdd2node[0],
+                                           self.__apply__(op, bdd1, bdd2, n1, bdd2node[1]),
+                                           self.__apply__(op, bdd1, bdd2, n1, bdd2node[2]))
 
-            print stack
-            if flag[stack[-1][0]] == 2:
-                stack.pop()
+            else:
+                self.nodelist[thisnode] = (bdd2node[0],
+                                           self.__apply__(op, bdd1, bdd2, n1, bdd2node[1]),
+                                           self.__apply__(op, bdd1, bdd2, n1, bdd2node[2]))
+        else:
+            if len(bdd2node):
+                if not bdd2node[1]:
+                    if op in ('&', 'and'):
+                        self.nodelist[thisnode] = (self.varnum, False)
+                    else:  #suppose we never use xor
+                        self.nodelist[thisnode] = (bdd1node[0],
+                                                   self.__apply__(op, bdd1, bdd2, bdd1node[1], n2),
+                                                   self.__apply__(op, bdd1, bdd2, bdd1node[2], n2))
+                    #TODO:still need to implement xor
 
+                else:
+                    if op in ('|', 'or'):
+                        self.nodelist[thisnode] = (self.varnum, True)
+                    else: #suppose we never use xor
+                        self.nodelist[thisnode] = (bdd1node[0],
+                                                   self.__apply__(op, bdd1, bdd2, bdd1node[1], n2),
+                                                   self.__apply__(op, bdd1, bdd2, bdd1node[2], n2))
+                    #TODO:still need to implement xor
+
+            else:
+                self.nodelist[thisnode] = (bdd1node[0],
+                                           self.__apply__(op, bdd1, bdd2, bdd1node[1], n2),
+                                           self.__apply__(op, bdd1, bdd2, bdd1node[2], n2))
+
+        return thisnode
 
 
 bdd1 = BDD()
@@ -136,3 +211,4 @@ bdd1.construct('11X1')
 bdd2.construct('111X')
 
 bdd.apply('&', bdd1, bdd2)
+pass
